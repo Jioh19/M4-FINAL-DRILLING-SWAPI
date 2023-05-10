1502a13914139feb562035d18b9c7567aac9f3c9
Jioh19 <jioh1982@gmail.com>
2023-05-10 13:28:28 -0400

    Entrega base lista

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,36 @@
-let index = [true, true, true];
+const index = [true, true, true];
+const range = [0, 5, 11, 17];
+const promesas = [];
 
-function cargar(num) {
-	if(index[num-1]) {
-		for (let i = 0; i < 5; i++) {
-			buscar((num - 1) * 5 + 1 + i + index[num - 1], num);
+//! Supuestamente esta función debería esperar a que todos los fetch se cumplan antes de continuar.
+async function cargar(num) {
+	if (index[num - 1]) {
+		for (let i = range[num - 1]; i < range[num]; i++) {
+			promesas[i] = buscar(i + 1, num);
 		}
-		index[num-1] = false;
+		index[num - 1] = false;
 	}
+	await Promise.all(promesas).then(() => {
+		console.log(promesas);
+	});
 }
 
+// TODO No entendí como implementar correctamente el fetch como para capturar correctamente el error
+// TODO del response.ok cuando este daba error de tipo 400
 async function buscar(num, origen) {
 	let dir = "https://swapi.dev/api/people/" + num + "/";
 	fetch(dir)
 		.then((response) => response.json())
 		.then((personaje) => {
-            createCard(personaje, origen);
-		});
+			if (personaje.detail != "Not found") {
+				createCard(personaje, origen);
+			}
+		})
 }
 
 function createCard(personaje, num) {
-    const node = document.querySelector(`.sector-${num}`);
-    node.innerHTML += `<div class="card card-${num}">
+	const node = document.querySelector(`.sector-${num}`);
+	node.innerHTML += `<div class="card card-${num}">
 						<div class="btnDiv">
 							<button class="btn btn-${num}"></button>
 						</div>
