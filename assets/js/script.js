15eec718419dd5190b59c4582d95f5f9ff9036f3
Jioh19 <jioh1982@gmail.com>
2023-05-10 23:44:28 -0400

    Entrega Final

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,11 @@ async function cargar(num) {
 			promesas[i] = buscar(i + 1, num);
 		}
 		index[num - 1] = false;
+		await Promise.all(promesas).then(() => {
+			document.querySelector(".modal-body").innerHTML = "";
+			modal.style.display = "block";
+		});
 	}
-	await Promise.all(promesas).then(() => {
-		console.log(promesas);
-	});
 }
 
 // TODO No entendí como implementar correctamente el fetch como para capturar correctamente el error
@@ -20,16 +21,23 @@ async function cargar(num) {
 async function buscar(num, origen) {
 	let dir = "https://swapi.dev/api/people/" + num + "/";
 	fetch(dir)
-		.then((response) => response.json())
-		.then((personaje) => {
-			if (personaje.detail != "Not found") {
-				createCard(personaje, origen);
+		.then((response) => {
+			if (response.ok) {
+				return response.json();
+			} else {
+				alert(`Personaje no encontrado\n${dir}`);
 			}
 		})
+		.then((personaje) => {
+			if (personaje) {
+				createCard(personaje, origen);
+			}
+		});
 }
 
 function createCard(personaje, num) {
 	const node = document.querySelector(`.sector-${num}`);
+	const modal = document.querySelector('.modal-body');
 	node.innerHTML += `<div class="card card-${num}">
 						<div class="btnDiv">
 							<button class="btn btn-${num}"></button>
@@ -39,4 +47,36 @@ function createCard(personaje, num) {
 							<p>Estatura: ${personaje.height}, Peso: ${personaje.mass} kg.</p>
 						</div>
 					</div>`;
+	modal.innerHTML += `<div class="card card-${num}">
+						<div>
+							<h2>${personaje.name}</h2>
+							<p>Estatura: ${personaje.height}, Peso: ${personaje.mass} kg.</p>
+						</div>
+					</div>`;
 }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+	modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+	modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+};
